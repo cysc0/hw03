d0495034060f604da9f567838dc1b8e090b249b6
Chris Gast <cisco@mothership.local>
2019-01-25 02:18:49 -0500

    deploy fix?

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,45 @@
 #!/bin/bash
 
-export MIX_ENV=prod
+# Source: Nat Tuck - http://khoury.neu.edu/~ntuck/courses/2019/01/cs4550/notes/05-frontend-intro/
+
 export PORT=4790
-export NODEBIN=`pwd`/assets/node_modules/.bin
-export PATH="$PATH:$NODEBIN"
+export MIX_ENV=prod
+# export NODEBIN=`pwd`/assets/node_modules/.bin
+# export PATH="$PATH:$NODEBIN"
+export GIT_PATH=/home/practice/hw03
+
+PWD=`pwd`
+if [ $PWD != $GIT_PATH ]; then
+	echo "Error: Must check out git repo to $GIT_PATH"
+	echo "  Current directory is $PWD"
+	exit 1
+fi
+
+if [ $USER != "practice" ]; then
+	echo "Error: must run as user 'practice'"
+	echo "  Current user is $USER"
+	exit 2
+fi
 
 echo "Building..."
 
 mix deps.get
-mix compile
 (cd assets && npm install)
-(cd assets && webpack --mode production)
+(cd assets && ./node_modules/brunch/bin/brunch b -p)
 mix phx.digest
+MIX_ENV=prod mix release --env=prod
 
-echo "Generating release..."
-mix release
+mkdir -p ~/www
+mkdir -p ~/old
 
-#echo "Stopping old copy of app, if any..."
-#_build/prod/rel/practice/bin/practice stop || true
+NOW=`date +%s`
+if [ -d ~/www/hw03 ]; then
+	echo mv ~/www/hw03 ~/old/$NOW
+	mv ~/www/hw03 ~/old/$NOW
+fi
 
-echo "Starting app..."
+mkdir -p ~/www/hw03
+REL_TAR=~/src/hw03/_build/prod/rel/memory/releases/0.0.1/hw03.tar.gz
+(cd ~/www/hw03 && tar xzvf $REL_TAR)
 
-_build/prod/rel/practice/bin/practice foreground
-
+#. start.sh
