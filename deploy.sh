3ee856d36ea1d98a555da2535839f40101f1a0d5
Chris Gast <cisco@mothership.local>
2019-01-25 04:23:55 -0500

    final

--- deploy.sh
+++ deploy.sh
@@ -19,8 +19,5 @@ mix phx.digest
 echo "Generating release..."
 mix release
 
-echo "Stopping old copy of app, if any..."
-_build/prod/rel/practice/bin/practice stop || true
-
-echo "Starting app..."
-_build/prod/rel/practice/bin/practice start
+echo "Starting app...";
+PORT=4790 MIX_ENV=prod mix phx.server;
--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-export MIX_ENV=prod
-export PORT=4790
-
-echo "Stopping old copy of app, if any..."
-_build/prod/rel/practice/bin/practice stop || true
-
-echo "Starting app..."
-_build/prod/rel/practice/bin/practice start
+PORT=4790 MIX_ENV=prod mix phx.server;
