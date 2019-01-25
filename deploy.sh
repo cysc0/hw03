39777761061851fc6b229b87d281ef01cd3e52a8
Chris Gast <cisco@mothership.local>
2019-01-25 03:41:36 -0500

    new deploy

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,16 @@ echo "Building..."
 
 mix deps.get
 mix compile
-(cd assets && npm install)
-(cd assets && webpack --mode production)
+cd assets
+npm install
+webpack --mode production
 mix phx.digest
 
 echo "Generating release..."
 mix release
 
-#echo "Stopping old copy of app, if any..."
-#_build/prod/rel/practice/bin/practice stop || true
+echo "Stopping old copy of app, if any..."
+_build/prod/rel/practice/bin/practice stop || true
 
 echo "Starting app..."
-
 _build/prod/rel/practice/bin/practice start
